0dae46d8b1277967bffcf5d3a53af3714bd63ffb
philarmstead <philarmstead@gmail.com>
2025-04-24 14:23:49 +0100

    Add Windows support

--- src/process.h
+++ src/process.h
@@ -4,6 +4,48 @@
 
 #pragma once
 
+#ifdef _WIN32
+
+#include <minwindef.h>
+#include <stdio.h>
+#include <tlhelp32.h>
+
+
+#define INVALID_HANDLE_VALUE ((HANDLE) (LONG_PTR)-1)
+#define WINBASEAPI DECLSPEC_IMPORT
+
+WINBASEAPI WINBOOL WINAPI CloseHandle(HANDLE hObject);
+
+WINBASEAPI HANDLE WINAPI OpenProcess(DWORD dwDesiredAccess, WINBOOL bInheritHandle, DWORD dwProcessId);
+
+static inline int findProcessByName(const char *processName) {
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (INVALID_HANDLE_VALUE == hSnapshot) { return 0; }
+
+	PROCESSENTRY32 pe;
+	pe.dwSize = sizeof(PROCESSENTRY32);
+
+	BOOL hResult = Process32First(hSnapshot, &pe);
+
+	int pid = 0;
+	while (hResult) {
+		if (strcmp(processName, pe.szExeFile) == 0) {
+			pid = pe.th32ProcessID;
+			break;
+		}
+		hResult = Process32Next(hSnapshot, &pe);
+	}
+
+	CloseHandle(hSnapshot);
+	return pid;
+}
+
+static inline HANDLE getProcessFileDescriptor(int *pid) {
+	*pid = findProcessByName("FFX.exe");
+	return OpenProcess(PROCESS_ALL_ACCESS, TRUE, *pid);
+}
+
+#else
 #include <fcntl.h>
 #include <stdlib.h>
 
@@ -18,7 +60,7 @@ static inline long openProcess() {
 	return pid;
 }
 
-static inline int getProcessFileDescriptor() {
+static inline uint64_t getProcessFileDescriptor() {
 	const long pid = openProcess();
 	if (pid) {
 		char memoryPath[32];
@@ -28,3 +70,4 @@ static inline int getProcessFileDescriptor() {
 
 	return -1;
 }
+#endif
